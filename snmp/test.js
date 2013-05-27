e886b942f068679ea54d4ca4864ea401546e6ce7
Steve Trease <stevetrease@gmail.com>
2013-05-27 07:26:57 +0100

    More testing

---
+++ snmp/test.js
@@ -0,0 +1,28 @@
+
+function snmp_data () {
+	this.a = null;
+	this.b = null;	
+}
+
+var test = [];
+
+
+
+var item = new snmp_data();
+item.a = 1;
+item.b = 1;
+test.push(item);
+var item = new snmp_data();
+item.a = 1;
+item.b = 1;
+test.push(item);
+var item = new snmp_data();
+item.a = 1;
+item.b = 1;
+test.push(item);
+
+
+
+for (var key in test) {
+	console.log(key, test[0].a, test[0].b);
+}
