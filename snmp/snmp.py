f050b19b0a6158cf96aaafa48a5f32fc902dbb67
Steve Trease <stevetrease@gmail.com>
2013-06-14 14:33:49 +0100

    Removed 1 second sleep

--- snmp/snmp.py
+++ snmp/snmp.py
@@ -1,5 +1,3 @@
-mode = 1
-
 import netsnmp
 import time
 import datetime
@@ -29,24 +27,31 @@ data_down = [ 0, 0, 0, 0, 0 ]
 bytes_up_old = int(session.get(vars1)[0])
 bytes_down_old = int(session.get(vars2)[0])
 
+oldtime = datetime.datetime.now()
+
 while mqttc.loop() == 0:
+	
+	
 	bytes_up_new = int(session.get(vars1)[0])
 	bytes_down_new = int(session.get(vars2)[0])
 	bytes_up_diff = bytes_up_new - bytes_up_old
 	bytes_down_diff = bytes_down_new - bytes_down_old
 
-	mbps_total = round((bytes_down_diff + bytes_up_diff) * 8.0 / 1000000.0, 3)
-	mbps_down = round((bytes_down_diff) * 8.0 / 1000000.0, 3)
-	mbps_up = round((bytes_up_diff) * 8.0 / 1000000.0, 3)
+	newtime = datetime.datetime.now()
+	timediff = newtime - oldtime
 
+	mbps_total = round((bytes_down_diff + bytes_up_diff) * 8.0 / 1000000.0 / timediff.total_seconds() , 3)
+	mbps_down = round((bytes_down_diff) * 8.0 / 1000000.0 / timediff.total_seconds(), 3)
+	mbps_up = round((bytes_up_diff) * 8.0 / 1000000.0 / timediff.total_seconds(), 3)
 
 	mqttc.publish("sensors/snmp/router/up", str(bytes_up_new))
 	mqttc.publish("sensors/snmp/router/down", str(bytes_down_new))
 	
 	# print current bandwidth on bottom line of LCD
-	mqttc.publish("LCD/1/line/0", "{:5.2f}".format(mbps_total))
-
+	mqttc.publish("LCD/1/line/1", "{:5.2f}".format(mbps_total))
 
 	bytes_up_old = bytes_up_new
 	bytes_down_old = bytes_down_new
-	time.sleep(1)
+	oldtime = newtime
+	
+	# time.sleep(1)
