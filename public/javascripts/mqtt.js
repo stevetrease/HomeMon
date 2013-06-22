13d5128a2bf6df68cd32f5d0cabd99e4ed91ba34
Steve Trease <stevetrease@gmail.com>
2013-06-22 14:20:09 +0100

    Changed host and port.

--- public/javascripts/mqtt.js
+++ public/javascripts/mqtt.js
@@ -4,7 +4,7 @@
 	
 	var updates = 0;
 
-	var socket = io.connect("http://silver.trease.eu:3000/mqtt");
+	var socket = io.connect("http://www.trease.eu:8500/mqtt");
 	socket.on('data', function(data) { 
 		// console.log("Message received " + data.topic + " of " + data.value);
 
