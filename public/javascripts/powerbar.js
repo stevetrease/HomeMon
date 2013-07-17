d6011ecf2e1c7ac22a80f9415ac23734264211ad
Steve Trease <stevetrease@gmail.com>
2013-07-17 18:16:48 +0100

    Added Power Bar page

---
+++ public/javascripts/powerbar.js
@@ -0,0 +1,54 @@
+	ElementExists = function(id) {
+    		return !!document.getElementById(id);
+	};
+	BeginsWith = function(needle, haystack) {
+		return (haystack.substr(0, needle.length) == needle);
+	}
+
+    // Load the Visualization API and the piechart package.
+    google.load('visualization', '1.0', {'packages':['corechart','table']});
+
+	// Set a callback to run when the Google Visualization API is loaded.
+	google.setOnLoadCallback(drawChart);
+
+
+    function drawChart() {
+	  	var data = new google.visualization.DataTable();	           
+
+	  	data.addColumn('string', 'Label');
+	  	data.addRow();
+		
+		// Instantiate and draw our chart	 
+		var options = {height: 300,
+		               legend: {position: 'none'},
+		               animation: { duration: 250, easing: 'in'},
+		               isStacked: true};
+		
+		var chart = new google.visualization.BarChart(document.getElementById('powerbar'));
+		var table = new google.visualization.Table(document.getElementById('powertable'));
+
+
+		var updates = 0;
+		var socket = io.connect("https://www.trease.eu:8500/powerbar");
+		socket.on('data', function(message) { 
+			
+			var done = false; 
+			for (var i = 0; i < data.getNumberOfColumns(); i++) {
+				if (data.getColumnLabel(i) == message.topic) {
+					data.setCell(0, i, parseInt(message.value, 10));
+					done = true;
+				}
+			}
+			if (done == false) {
+				data.addColumn('number', message.topic);
+			}					
+			chart.draw(data, options);
+			
+			var dt = new Date(); 
+			document.getElementById("time").innerHTML= dt.toLocaleTimeString();
+			document.getElementById("updates").innerHTML= ++updates;
+
+			table.draw(data, {showRowNumber: false});
+			
+		});
+	}
