c74793065f0ba79177fe0378792e16cae38d858d
Steve Trease <stevetrease@gmail.com>
2014-11-05 12:25:33 +0000

    Added tab to display push messages.

---
+++ public/javascripts/pushmessage.js
@@ -0,0 +1,19 @@
+	ElementExists = function(id) {
+    		return !!document.getElementById(id);
+	};
+		
+	var updates = 0;
+
+	var socket = io.connect("https://www.trease.eu:8500/pushmessage");
+	socket.on('data', function(data) { 
+		var table=document.getElementById("messagetable");
+			
+		var row = table.insertRow(0);
+		var cell = row.insertCell(0);
+		cell.innerHTML = data.topic;
+					
+		// print the time the refresh happened
+		var dt = new Date(); 
+		document.getElementById("time").innerHTML = dt.toLocaleTimeString();
+		document.getElementById("updates").innerHTML = ++updates;
+	});
