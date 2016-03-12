21b1b703c47c7c9c63b22562c3675a94c83d5c03
Steve <stevetrease@gmail.com>
2016-03-12 18:49:33 +0000

    code changes to improve codacity score

--- homemon-server/homemon-server.js
+++ homemon-server/homemon-server.js
@@ -1,76 +1,76 @@
 console.log("process.env.NODE_ENV:" + process.env.NODE_ENV);
 switch (process.env.NODE_ENV) {
-	case 'development':
+	case "development":
 		console.log ("development mode");
-		var config = require('../config.json');
+		var config = require("../config.json");
 		break;
-	case 'production':
+	case "production":
 	default:	
 		console.log ("production mode");
-		var config = require('../config.json');
+		var config = require("../config.json");
 }
 
 
 // function to if a string starts with nother one
 String.prototype.beginsWith = function (string) {
-    return(this.indexOf(string) === 0);
+	return(this.indexOf(string) === 0);
 };
 
-var names = require('../names.json');
+var names = require("../names.json");
 var topicHistory = {};
 var topicHistoryTimeStamp = {};
 
 
-var fs = require('fs');
-var path = require('path');
-var express = require('express');
+var fs = require("fs");
+var path = require("path");
+var express = require("express");
 var app = express();
-var http = require('http');
+var http = require("http");
 var server = http.createServer(app);
-var io = require('socket.io').listen(server, {'log level': 1});
+var io = require("socket.io").listen(server, {"log level": 1});
 
-app.set('port', config.host_port);
-app.set('host', "127.0.0.1");
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.set("port", config.host_port);
+app.set("host", "127.0.0.1");
+app.set("views", __dirname + "/views");
+app.set("view engine", "jade");
 app.use(express.logger());
-app.use(express.favicon(__dirname + '/public/favicon.ico'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.favicon(__dirname + "/public/favicon.ico"));
+app.use(express.static(path.join(__dirname, "public")));
 
-var routes = require('./routes');
-var page_mqtt = require('./routes/page_mqtt');
-var page_mqttstats = require('./routes/page_mqttstats');
-var page_power = require('./routes/page_power');
-var page_powerbar = require('./routes/page_powerbar');
-var page_pushmessage = require('./routes/page_pushmessage');
-var page_sensors = require('./routes/page_sensors');
-var page_sensors2 = require('./routes/page_sensors2');
-var page_snmp = require('./routes/page_snmp');
+var routes = require("./routes");
+var page_mqtt = require("./routes/page_mqtt");
+var page_mqttstats = require("./routes/page_mqttstats");
+var page_power = require("./routes/page_power");
+var page_powerbar = require("./routes/page_powerbar");
+var page_pushmessage = require("./routes/page_pushmessage");
+var page_sensors = require("./routes/page_sensors");
+var page_sensors2 = require("./routes/page_sensors2");
+var page_snmp = require("./routes/page_snmp");
 
 
 
-app.get('/', routes.index);
-app.get('/mqtt', page_mqtt.page);
-app.get('/pushmessage', page_pushmessage.page);
-app.get('/mqttstats', page_mqttstats.page);
-app.get('/power', page_power.page);
-app.get('/powerbar', page_powerbar.page);
-app.get('/pushmessage', page_pushmessage.page);
-app.get('/sensors', page_sensors.page);
-app.get('/sensors2', page_sensors2.page);
-app.get('/snmp', page_snmp.page);
+app.get("/", routes.index);
+app.get("/mqtt", page_mqtt.page);
+app.get("/pushmessage", page_pushmessage.page);
+app.get("/mqttstats", page_mqttstats.page);
+app.get("/power", page_power.page);
+app.get("/powerbar", page_powerbar.page);
+app.get("/pushmessage", page_pushmessage.page);
+app.get("/sensors", page_sensors.page);
+app.get("/sensors2", page_sensors2.page);
+app.get("/snmp", page_snmp.page);
 
 
 // and finally a 404
 app.use(function(req, res, next){
 	res.send(404, "This is not the webpage you are looking for.");
 });
-server.listen(config.host_port, 'localhost');
-console.log('listening on port ' + config.host_port);
+server.listen(config.host_port, "localhost");
+console.log("listening on port " + config.host_port);
 
 
-io.sockets.on('connection', function (socket) {
-	socket.on('subscribe', function(data) {
+io.sockets.on("connection", function (socket) {
+	socket.on("subscribe", function(data) {
 		console.log (socket.id + " on " + socket.request.connection.remoteAddress + " subscribed to room " + data.room );
 		socket.join(data.room);
 		
@@ -92,108 +92,107 @@ io.sockets.on('connection', function (socket) {
 });
 
 
-var nsp = io.of('/');
-nsp.on('connection', function(socket){
+var nsp = io.of("/");
+nsp.on("connection", function(socket){
 	console.log(socket.id + " on " + socket.request.connection.remoteAddress + " connected connected to /");
 });
 
 
 
-var mqtt = require('mqtt');
+var mqtt = require("mqtt");
 var mqttclient = mqtt.connect(config.mqtt.host, config.mqtt.options);
 
-mqttclient.on('connect', function() {
-        // mqttclient.subscribe('jsonsensors');
-        mqttclient.subscribe('#');
-        mqttclient.subscribe('$SYS/#');
-        mqttclient.on('message', function(topic, message) {    
-	        var value = Number(message);
-	        messageString = value.toString();
-	        // console.log (topic + "     " + message.toString());
-	        
-	        if (topic == "push/alert") {
-				io.sockets.in("pushmessage").emit('data', { topic: message.toString() });	        
-		    }
-		    
-	        if (topic == "jsonsensors") {
-		        // var messageData = JSON.parse(message.toString());
-				//io.sockets.in("mqtt").emit('data', { topic: messageData.topic, value: messageData.value });				
+mqttclient.on("connect", function() {
+		// mqttclient.subscribe("jsonsensors");
+		mqttclient.subscribe("#");
+		mqttclient.subscribe("$SYS/#");
+		mqttclient.on("message", function(topic, message) {    
+			var value = Number(message);
+			messageString = value.toString();
+			// console.log (topic + "     " + message.toString());
+
+			if (topic == "push/alert") {
+				io.sockets.in("pushmessage").emit("data", { topic: message.toString() });	        
 			}
-			
+
+			if (topic == "jsonsensors") {
+				// var messageData = JSON.parse(message.toString());
+				//io.sockets.in("mqtt").emit("data", { topic: messageData.topic, value: messageData.value });				
+			}
+
 			if (topic.beginsWith("$SYS/")) {
-				io.sockets.in("mqttstats").emit('data', { topic: topic, value: message.toString() })
+				io.sockets.in("mqttstats").emit("data", { topic: topic, value: message.toString() })
 				return;
 			}
-			
+
 			if (topic.beginsWith("snmp")) {
 				// console.log(message.toString());
-				io.sockets.in("snmp").emit('data', { message: message.toString() });	
+				io.sockets.in("snmp").emit("data", { message: message.toString() });	
 			}
-			
+
 			// retain messages so that we have starting data for new clients
 			if (topic.beginsWith("sensors/")) {
 				var time = new Date();
 				topicHistoryTimeStamp[topic] = time;
 				topicHistory[topic] = message;
-			}	        
-			
+			}
+
 			if (topic.beginsWith("sensors/power")) {
 				var name = null;
 				if (names[topic] != undefined) name = names[topic].name;
 				messageString = value.toFixed(0);
-				io.sockets.in("power").emit('data', { topic: topic, value: messageString, name: name });	
+				io.sockets.in("power").emit("data", { topic: topic, value: messageString, name: name });
 			}
-			
+
 			if (topic.beginsWith("sensors/power") || topic.beginsWith("rate/sensors/snmp/router/total")
 			|| topic.beginsWith("sensors/power") || topic.beginsWith("sensors/co/") || topic.beginsWith("sensors/co2/")
 			|| topic.beginsWith("sensors/no2") || topic.beginsWith("sensors/pressure")) {
 				var name = null;
 				if (names[topic] != undefined) name = names[topic].name;
-				io.sockets.in("sensors").emit('data', { topic: topic, value: messageString, name: name });	
+				io.sockets.in("sensors").emit("data", { topic: topic, value: messageString, name: name });
 			}
-			
+
 			if (topic.beginsWith("sensors/co/") || topic.beginsWith("sensors/co2/")
 			|| topic.beginsWith("sensors/temperature/") || topic.beginsWith("sensors/humidity/")
 			|| topic.beginsWith("sensors/no2") || topic.beginsWith("sensors/pressure")) {
 				var name = null;
 				if (names[topic] != undefined) name = names[topic].name;
-				io.sockets.in("sensors2").emit('data', { topic: topic, value: messageString, name: name });	
+				io.sockets.in("sensors2").emit("data", { topic: topic, value: messageString, name: name });
 			}
-
 
 			if (topic.beginsWith("sensors/iosbattery")) {
 				var name = null;
 				if (names[topic] != undefined) name = names[topic].name;
 				var p = value * 100;
 				messageString = p.toFixed(0);
-				io.sockets.in("sensors").emit('data', { topic: topic, value: messageString, name: name });	
+				io.sockets.in("sensors").emit("data", { topic: topic, value: messageString, name: name });
 			}
 
 			if (topic.beginsWith("sensors/humidity")) {
 				var name = null;
 				if (names[topic] != undefined) name = names[topic].name;
 				messageString = value.toFixed(0) + "%";
-				io.sockets.in("sensors").emit('data', { topic: topic, value: messageString, name: name });	
-				io.sockets.in("humidity").emit('data', { topic: topic, value: messageString, name: name });	
+				io.sockets.in("sensors").emit("data", { topic: topic, value: messageString, name: name });
+				io.sockets.in("humidity").emit("data", { topic: topic, value: messageString, name: name });
 			}
-			
+
 			if (topic.beginsWith("sensors/temperature") || topic.beginsWith("sensors/boiler")) {
 				var name = null;
 				if (names[topic] != undefined) name = names[topic].name;
 				messageString = value.toFixed(1);
-				io.sockets.in("sensors").emit('data', { topic: topic, value: messageString, name: name });	
-				io.sockets.in("temperature").emit('data', { topic: topic, value: messageString, name: name });	
+				io.sockets.in("sensors").emit("data", { topic: topic, value: messageString, name: name });
+				io.sockets.in("temperature").emit("data", { topic: topic, value: messageString, name: name });
 			}
-			
+
 			if (topic == "cumulative/hour/sensors/power/0" || topic == "cumulative/daily/sensors/power/0") {
-				io.sockets.in("sensors").emit('data', { topic: topic, value: messageString });
+				io.sockets.in("sensors").emit("data", { topic: topic, value: messageString });
 			}
-			
+
 			if (topic.beginsWith("cumulative/hour/sensors/power/") || topic.beginsWith("cumulative/daily/sensors/power/")) {
-				io.sockets.in("power_cumulative").emit('data', { topic: topic, value: messageString });
+				io.sockets.in("power_cumulative").emit("data", { topic: topic, value: messageString });
 			}
 
 			// emit everything to the mqtt room
-			io.sockets.in("mqtt").emit('data', { topic: topic, value: message.toString() });			
-       });
+			io.sockets.in("mqtt").emit("data", { topic: topic, value: message.toString() });
+		});
 });
--- homemon-stats/homemon-stats.js
+++ homemon-stats/homemon-stats.js
@@ -6,7 +6,7 @@ switch (process.env.NODE_ENV) {
 		var config = require('../config.json');
 		break;
 	case 'production':
-	default:	
+	default:
 		console.log ("production mode");
 		var config = require('../config.json');
 }
@@ -14,7 +14,7 @@ switch (process.env.NODE_ENV) {
 
 
 var redis = require('redis')
-   ,redisClient = redis.createClient(parseInt(config.redis.port,10), config.redis.host);
+	,redisClient = redis.createClient(parseInt(config.redis.port,10), config.redis.host);
 
 
 
@@ -24,28 +24,28 @@ BeginsWith = function(needle, haystack) {
 
 
 
-var records_hourly = {};
-var records_daily = {};
-var records_lasttime = {};
-var records_lastvalue = {};
+var recordsHourly = {};
+var recordsDaily = {};
+var recordsLastTime = {};
+var recordsLastValue = {};
 
 // get last stored values from Redis
 // needs to be blocking to get everything initialised nicely when starting
 var execSync = require('sync-exec');
 
-var result = execSync('redis-cli -h ' + config.redis.host + ' get records_hourly');
+var result = execSync('redis-cli -h ' + config.redis.host + ' get recordsHourly');
 console.log("Loading hourly records from redis...");
 try {
-	records_hourly = JSON.parse(result.stdout);
+	recordsHourly = JSON.parse(result.stdout);
 	console.log ("records loaded");
 } catch (error) {
 	console.log ("no records loaded");
 }
 
-result = execSync('redis-cli -h ' + config.redis.host + ' get records_daily');
+result = execSync('redis-cli -h ' + config.redis.host + ' get recordsDaily');
 console.log("Loading daily records from redis...");
 try {
-	records_daily = JSON.parse(result.stdout);
+	recordsDaily = JSON.parse(result.stdout);
 	console.log ("records loaded");
 } catch (error) {
 	console.log ("no records loaded");
@@ -65,57 +65,57 @@ mqttclient.on('connect', function() {
 	mqttclient.subscribe('sensors/temperature/+');
 	console.log('subscribing to sensors/temperature/+ on ' + config.mqtt.host);
 
-  	mqttclient.on('message', function(topic, message) {
+	mqttclient.on('message', function(topic, message) {
 		var currenttime = new Date();
 
 		// have we seen this mesage before? 
-		if(records_lasttime[topic] == undefined) {
-			records_lasttime[topic] = new Date();
+		if(recordsLastTime[topic] == undefined) {
+			recordsLastTime[topic] = new Date();
 		}
-		if(records_hourly[topic] == undefined) {
-			records_hourly[topic] = 0;
+		if(recordsHourly[topic] == undefined) {
+			recordsHourly[topic] = 0;
 		}
-		if(records_daily[topic] == undefined) {
-			records_daily[topic] = 0;
+		if(recordsDaily[topic] == undefined) {
+			recordsDaily[topic] = 0;
 		}
-		if(records_lastvalue[topic] == undefined) {
-			records_lastvalue[topic] = 0;
+		if(recordsLastValue[topic] == undefined) {
+			recordsLastValue[topic] = 0;
 		}
 
 		// publish current power stats for LCD display
 		if (BeginsWith("sensors/power/0", topic)) {
 			mqttclient.publish("LCD/1/line/0", message + "  " + 
-			records_hourly[topic].toFixed(1) + "  " + records_daily[topic].toFixed(1));
+			recordsHourly[topic].toFixed(1) + "  " + recordsDaily[topic].toFixed(1));
 		}
-		
+
 
 		// different hour?
-		if (records_lasttime[topic].getHours() != currenttime.getHours()) {					
+		if (recordsLastTime[topic].getHours() != currenttime.getHours()) {
 			// and historize	
 			var messages = {
-				time: records_lasttime[topic].getTime(),
-				value: records_hourly[topic]	
+				time: recordsLastTime[topic].getTime(),
+				value: recordsHourly[topic]	
 			}
 			redisClient.zadd(topic + "_hourly", currenttime.getTime(), JSON.stringify(messages));
-			records_hourly[topic] = 0;
+			recordsHourly[topic] = 0;
 		}
 		// different day?
-		if (records_lasttime[topic].getDate() != currenttime.getDate()) {
+		if (recordsLastTime[topic].getDate() != currenttime.getDate()) {
 			// and historize
 			var messages = {
-				time: records_lasttime[topic].getTime(),
-				value: records_daily[topic]	
+				time: recordsLastTime[topic].getTime(),
+				value: recordsDaily[topic]	
 			}
 			redisClient.zadd(topic + "_daily", currenttime.getTime(), JSON.stringify(messages));
 			// reset accumulator
-			records_daily[topic] = 0;
+			recordsDaily[topic] = 0;
 		}
 
 
 		// Run this code for power records
 		// calculate cumulative power used in KWh
-		var duration = (currenttime - records_lasttime[topic]); // in milli seconds
-		
+		var duration = (currenttime - recordsLastTime[topic]); // in milli seconds
+
 
 		// Record is power
 		if (BeginsWith("sensors/power/", topic)) {	
@@ -125,17 +125,17 @@ mqttclient.on('connect', function() {
 				console.log ("NAN powerused: " + topic);
 				powerused = 0;
 			}
-			records_lasttime[topic] = currenttime;
-			records_hourly[topic] += powerused;
-			records_daily[topic] += powerused;
-			records_lastvalue[topic] = parseInt(message, 10);
-			// console.log("topic:", topic, " duration ", duration, " period ", powerused, " hour ", records_hourly[topic], "daily ", records_daily[topic]);
-			
+			recordsLastTime[topic] = currenttime;
+			recordsHourly[topic] += powerused;
+			recordsDaily[topic] += powerused;
+			recordsLastValue[topic] = parseInt(message, 10);
+			// console.log("topic:", topic, " duration ", duration, " period ", powerused, " hour ", recordsHourly[topic], "daily ", recordsDaily[topic]);
+
 			// publish new data
-			mqttclient.publish("cumulative/hour/" + topic, records_hourly[topic].toFixed(2));
-			mqttclient.publish("cumulative/daily/" + topic, records_daily[topic].toFixed(2));
-			
-			
+			mqttclient.publish("cumulative/hour/" + topic, recordsHourly[topic].toFixed(2));
+			mqttclient.publish("cumulative/daily/" + topic, recordsDaily[topic].toFixed(2));
+
+
 			var v = powerused.toFixed(10);
 			if (!isNaN(v)) {	
 				var time = new Date();
@@ -147,43 +147,43 @@ mqttclient.on('connect', function() {
 				redisClient.hincrbyfloat("daily-" + topic, time, v);		
 			}
 		}
-		
+
 		// record is SNMP
 		if (BeginsWith("sensors/snmp", topic)) {
 			// console.log ("snmp - ", topic, duration);	
 			var snmp = parseInt(message, 10); 
-			var used = (snmp - records_lastvalue[topic]);
+			var used = (snmp - recordsLastValue[topic]);
 			if (used < 0) used = 0;
 			var rate = used * 8.0 / 1000000 * (duration / 1000.0); // in Mbps
-			records_lasttime[topic] = currenttime;
-			records_hourly[topic] += used;
-			records_daily[topic] += used;
-			records_lastvalue[topic] = parseInt(message, 10);
+			recordsLastTime[topic] = currenttime;
+			recordsHourly[topic] += used;
+			recordsDaily[topic] += used;
+			recordsLastValue[topic] = parseInt(message, 10);
 			// publish new data
-			mqttclient.publish("cumulative/hour/" + topic, records_hourly[topic].toFixed(0));
-			mqttclient.publish("cumulative/daily/" + topic, records_daily[topic].toFixed(0));
+			mqttclient.publish("cumulative/hour/" + topic, recordsHourly[topic].toFixed(0));
+			mqttclient.publish("cumulative/daily/" + topic, recordsDaily[topic].toFixed(0));
 			mqttclient.publish("rate/" + topic, rate.toFixed(2));
 		}
-		
+
 		// each time we get power/0 caculate then publish the "unknown power draw" and publish to power/U
 		if(topic == "sensors/power/0") {
 			var known = 0;
-			for (var key in records_lastvalue) {
+			for (var key in recordsLastValue) {
 				// only it it matches sensors/power/[0-9]
 				if (key.length == 15 && key != "sensors/power/0" && key != "sensors/power/U") {
-					// console.log(key, known, records_lastvalue[key]);
-					known += records_lastvalue[key]; 
+					// console.log(key, known, recordsLastValue[key]);
+					known += recordsLastValue[key]; 
 				}
 			}
-			var unknown = records_lastvalue["sensors/power/0"] - known;
+			var unknown = recordsLastValue["sensors/power/0"] - known;
 			if (unknown < 0) unknown = 0;
 			mqttclient.publish("sensors/power/U", unknown.toFixed(0));
 		}
-  	});
+	});
 });
 
 
-var MongoClient = require ("mongodb").MongoClient;
+// var MongoClient = require ("mongodb").MongoClient;
 
 
 
@@ -194,36 +194,34 @@ mqttclient2.on('connect', function() {
 	mqttclient2.subscribe('sensors/#');
 	console.log('subscribing to sensors/# on ' + config.mqtt.host);
 
-  	mqttclient2.on('message', function(topic, message) {
-	  	var time = new Date();
-	  	var jsonMessage = {
-		  	topic: topic,
-		  	value: Number(message.toString()),
-		  	timeStamp: Number(time)
-	  	};
+	mqttclient2.on('message', function(topic, message) {
+		var time = new Date();
+		var jsonMessage = {
+			topic: topic,
+			value: Number(message.toString()),
+			timeStamp: Number(time)
+		};
 
-		mqttclient2.publish("jsonsensors", JSON.stringify(jsonMessage)); 	
-		
+		mqttclient2.publish("jsonsensors", JSON.stringify(jsonMessage));
 
-		MongoClient.connect (config.mongo.host, function (err, db) {
-			db.collection ('homedata').insertOne (jsonMessage);	
-			db.close ();
-		});
+		// MongoClient.connect (config.mongo.host, function (err, db) {
+		// 	db.collection ('homedata').insertOne (jsonMessage);
+		// 	db.close ();
+		// });
 	});
 });
-	  
-	  	
+
 
 
 
 savestate = function() {
 	// console.log("saving to redis...");
-	redisClient.set("records_hourly", JSON.stringify(records_hourly));
-	redisClient.set("records_daily", JSON.stringify(records_daily));
-	redisClient.set("records_lasttime", JSON.stringify(records_lasttime));
+	redisClient.set("recordsHourly", JSON.stringify(recordsHourly));
+	redisClient.set("recordsDaily", JSON.stringify(recordsDaily));
+	redisClient.set("recordsLastTime", JSON.stringify(recordsLastTime));
 };
 
 // frequently store cumulative date to preserve it across restarts, etc.
 var savePeriod = 65; // in seconds
 setInterval (savestate, savePeriod * 1000);
-process.on('SIGUSR1', savestate);					// and save on signal
+process.on('SIGUSR1', savestate);	// and save on signal
